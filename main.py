64a1959188897965129fe7fe082b4783effdacd4
Pemidi <rashidi.peyman@gmail.com>
2021-01-11 14:53:19 +0330

    cities crawler added

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-
+import sys
 
 #get adv_links
 #stop crawl when adv pages finished
 
 def get_page(url, start = 0):
     try:
-        respone = requests.get(url.format(str(start)))
+        respone = requests.get(url+str(start))
     except:
         return None
     print(f"Status code : {respone.status_code}, link : {respone.url}")
@@ -17,17 +17,20 @@ def get_page(url, start = 0):
 
 def find_links(html_doc):
 
-    soup = BeautifulSoup(html_doc)
+    soup = BeautifulSoup(html_doc, 'html.parser')
     return soup.findAll('a', attrs={'class':'hdrlnk'})
-        
+    
 
-def start_crawl(url):
+def start_crawl_cites(url):
     start = 0
     crawl = True
     adv_links = list()
 
     while crawl:
         response = get_page(url, start)
+        if response is None:
+            crawl = False
+            continue
         new_links = find_links(response.text)
         adv_links.extend(new_links)
         start += 120
@@ -35,14 +38,26 @@ def start_crawl(url):
 
     return adv_links
 
+def start_crawl():
+    cites = ['london', 'berlin']
 
+    link = 'https://{}.craigslist.org/d/apartments-housing-for-rent/search/brx/apa?s='
+
+    for city in cites:
+        result = start_crawl_cites(link.format(city))
+        print(f"{city} : {len(result)}")
+
+
+def get_pages_data():
+    raise NotImplementedError()
 
 
 if __name__ == "__main__":
-    link = 'https://poconos.craigslist.org/d/apartments-housing-for-rent/search/apa?s={}'
     
+    switch = sys.arg[1]
 
-    links = start_crawl(link)
-    print('total:', len(links))    
-
+    if switch == 'find_links':
+        start_crawl()
+    elif switch == 'extract_pages':
+        get_pages_data()
     

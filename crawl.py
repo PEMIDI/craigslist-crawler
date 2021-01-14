7ea9c7f13d0a27202694c49f85f5f15145f03bb9
Pemidi <rashidi.peyman@gmail.com>
2021-01-14 11:04:26 +0330

    store func added

--- crawl.py
+++ crawl.py
@@ -1,7 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
 import json
 from abc import ABC, abstractmethod
+from bs4 import BeautifulSoup
 
 class BaseCrawler(ABC):
 
@@ -9,52 +9,53 @@ class BaseCrawler(ABC):
     def start(self):
         pass
 
-    # @abstractclassmethod
-    # def store():
-    #     pass
+    @abstractmethod
+    def store(self):
+        pass
 
 
-class Link_crawler(BaseCrawler):
+class LinkCrawler(BaseCrawler):
 
-    def __init__(self, link, cities):
-        self.link = link
+    def __init__(self, url, cities):
+        self.url = url
         self.cities = cities
 
-    def get_page(self ,link, start=0):
+    def get_page(self, url, start):
         try:
-            response = requests.get(link+str(start))
+            response = requests.get(url+str(start))
         except:
             return None
 
-        print(f"Status code : {response.status_code} : {response.url}")
-
+        print(f"{response.status_code} | {response.url}")
         return response
 
-    def link_finder(self, html_doc):
+    def find_links(self, html_doc):
         soup = BeautifulSoup(html_doc, 'html.parser')
         return soup.findAll('a', attrs={'class':'hdrlnk'})
 
-    
     def crawl_cities(self, url):
-        start = 0
         crawl = True
-        adv_links = []
+        start = 0
+        adv_link = []
 
         while crawl:
-            response = self.get_page(link=url, start=start)
-            if response is None:
-                crawl = False
-                continue
-            new_links = self.link_finder(response.text)
-            adv_links.extend(new_links)
+            response = self.get_page(url, start)
+            new_links = self.find_links(response.text)
+            adv_link.extend(new_links)
             start += 120
             crawl = bool(len(new_links))
 
-        return adv_links
+        return adv_link
+
+    def store(self, data):
+        with open('storage/links.json', 'w') as f:
+            f.write(json.dumps(data))
 
     def start(self):
+        adv_links = []
         for city in self.cities:
-            result = self.crawl_cities(self.link.format(city))
-            print(f"{city} : {len(result)}")
+            result = self.crawl_cities(self.url.format(city))
+            print(f"{city} | {len(result)}")
+            adv_links.extend(result)
 
-    
+        self.store([li.get('href') for li in adv_links])
